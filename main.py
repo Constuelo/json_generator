13b7ac8d4d0bd2cb3caadfd4b7356ade0b1f2a56
Rory Ferguson <Rory.Ferguson@joules.com>
2019-11-11 09:54:36 +0000

    Rewrote directory paths & removed old lines

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ import os
 from pathlib import Path
 import json
 
-
-snippet_ext = '.json'  # Extension name for sublimes snippet files
 convert_dir = 'convert'  # Location for input files
 
 path = Path(os.path.dirname(__file__))
-content_path = Path(os.path.dirname(__file__) + convert_dir)
-list_path = os.listdir(content_path)
+content_path = Path(path).joinpath(convert_dir)
 
 dict_object = dict()
-
 
 for d, s, f in os.walk(content_path):
     for file in f:
